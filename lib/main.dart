577db43f89d0ef3a1e63cacbc5807fea6df304f6
lakshanmaheshka <maheshkalakshan@gmail.com>
2021-04-27 16:59:28 +0530

    added icons, splash screen and login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+
+import 'pages/login_page.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  // SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+  //   DeviceOrientation.portraitUp,
+  //   DeviceOrientation.portraitDown,
+  // ]);
   await Firebase.initializeApp();
 
   runApp(MyApp());
@@ -15,10 +21,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
     debugShowCheckedModeBanner: false,
-    title: title,
+    //title: title,
     theme: ThemeData(primarySwatch: Colors.blue),
-    home: MainPage(),
+    home: SplashScreen(),
   );
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => new _SplashScreenState();
+}
+
+
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override void initState() {
+    super.initState();
+    new Future.delayed( const Duration(seconds: 5), () =>
+        Navigator.pushReplacement( context,
+          MaterialPageRoute(builder: (context) => LoginPage()),
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Color(0xFFE9E4F0),
+      body: new Center(
+        child: Image.asset("assets/images/loop3s.gif",
+            gaplessPlayback: true,
+            fit: BoxFit.fill
+        ),
+
+      ),
+    );
+  }
 }
 
 class MainPage extends StatefulWidget {
