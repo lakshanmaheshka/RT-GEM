2d051f65337e1d04e39ada029d87491a937f7a15
lakshanmaheshka <maheshkalakshan@gmail.com>
2021-05-15 17:52:27 +0530

    after dart migrate

--- lib/main.dart
+++ lib/main.dart
@@ -3,89 +3,110 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:rt_gem/pages/main_page.dart';
+import 'package:rt_gem/sample/screens/login_screen.dart';
 import 'package:rt_gem/theme.dart';
 
 import 'pages/login_page.dart';
 import 'provider/email_sign_in.dart';
 import 'provider/google_sign_in.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // SystemChrome.setPreferredOrientations(<DeviceOrientation>[
-  //   DeviceOrientation.portraitUp,
-  //   DeviceOrientation.portraitDown,
-  // ]);
-  await Firebase.initializeApp();
+// Future main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   // SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+//   //   DeviceOrientation.portraitUp,
+//   //   DeviceOrientation.portraitDown,
+//   // ]);
+//   await Firebase.initializeApp();
+//
+//   runApp(MyApp());
+// }
 
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  static final String title = 'RT GEM';
-
-  @override
-  Widget build(BuildContext context) => MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (context) => GoogleSignInProvider()),
-      ChangeNotifierProvider(create: (context) => EmailSignInProvider()),
-    ],
-    child: MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: title,
-      theme: ThemeData(primarySwatch: Colors.deepOrange),
-      home: MainPage(),
-    ),
-  );
-}
-
-//Todo: Implement Splash Screen
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => new _SplashScreenState();
-}
-
-
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override void initState() {
-    super.initState();
-    new Future.delayed( const Duration(seconds: 5), () =>
-        Navigator.pushReplacement( context,
-          MaterialPageRoute(builder: (context) => MainPage()),
-        ));
-  }
-
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      backgroundColor: Color(0xFFE9E4F0),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              colors: <Color>[
-                CustomTheme.loginGradientStart,
-                CustomTheme.loginGradientEnd
-              ],
-              begin: FractionalOffset(0.0, 0.0),
-              end: FractionalOffset(1.0, 1.0),
-              stops: <double>[0.0, 1.0],
-              tileMode: TileMode.clamp),
-        ),
-        child: new Center(
-          child:
-             Image(
-            width:300,
-              height:
-              MediaQuery.of(context).size.height > 800 ? 191.0 : 300,
-              fit: BoxFit.fill,
-              image: const AssetImage('assets/images/splash.gif')),
-
-
-
-        ),
+    return MaterialApp(
+      title: 'FlutterFire Samples',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        brightness: Brightness.dark,
       ),
+      home: LoginScreen(),
     );
   }
 }
+
+
+// class MyApp extends StatelessWidget {
+//   static final String title = 'RT GEM';
+//
+//   @override
+//   Widget build(BuildContext context) => MultiProvider(
+//     providers: [
+//       ChangeNotifierProvider(create: (context) => GoogleSignInProvider()),
+//       ChangeNotifierProvider(create: (context) => EmailSignInProvider()),
+//     ],
+//     child: MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: title,
+//       theme: ThemeData(primarySwatch: Colors.deepOrange),
+//       home: MainPage(),
+//     ),
+//   );
+// }
+
+// //Todo: Implement Splash Screen
+// class SplashScreen extends StatefulWidget {
+//   @override
+//   _SplashScreenState createState() => new _SplashScreenState();
+// }
+//
+//
+//
+// class _SplashScreenState extends State<SplashScreen> {
+//   @override void initState() {
+//     super.initState();
+//     new Future.delayed( const Duration(seconds: 5), () =>
+//         Navigator.pushReplacement( context,
+//           MaterialPageRoute(builder: (context) => MainPage()),
+//         ));
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Scaffold(
+//       backgroundColor: Color(0xFFE9E4F0),
+//       body: Container(
+//         width: MediaQuery.of(context).size.width,
+//         height: MediaQuery.of(context).size.height,
+//         decoration: const BoxDecoration(
+//           gradient: LinearGradient(
+//               colors: <Color>[
+//                 CustomTheme.loginGradientStart,
+//                 CustomTheme.loginGradientEnd
+//               ],
+//               begin: FractionalOffset(0.0, 0.0),
+//               end: FractionalOffset(1.0, 1.0),
+//               stops: <double>[0.0, 1.0],
+//               tileMode: TileMode.clamp),
+//         ),
+//         child: new Center(
+//           child:
+//              Image(
+//             width:300,
+//               height:
+//               MediaQuery.of(context).size.height > 800 ? 191.0 : 300,
+//               fit: BoxFit.fill,
+//               image: const AssetImage('assets/images/splash.gif')),
+//
+//
+//
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,35 +1,35 @@
-import 'package:rt_gem/pages/login_page.dart';
-import 'package:rt_gem/provider/google_sign_in.dart';
-import 'package:rt_gem/widgets/background_painter.dart';
-import 'package:rt_gem/pages/home_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-class MainPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        body: StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            final provider = Provider.of<GoogleSignInProvider>(context);
-
-            if (provider.isSigningIn) {
-              return buildLoading();
-            } else if (snapshot.hasData) {
-              return HomePage();
-            } else {
-              return LoginPage();
-            }
-          },
-        ),
-      );
-
-  Widget buildLoading() => Stack(
-        fit: StackFit.expand,
-        children: [
-          CustomPaint(painter: BackgroundPainter()),
-          Center(child: CircularProgressIndicator()),
-        ],
-      );
-}
+// import 'package:rt_gem/pages/login_page.dart';
+// import 'package:rt_gem/provider/google_sign_in.dart';
+// import 'package:rt_gem/widgets/background_painter.dart';
+// import 'package:rt_gem/pages/home_page.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+//
+// class MainPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) => Scaffold(
+//         body: StreamBuilder(
+//           stream: FirebaseAuth.instance.authStateChanges(),
+//           builder: (context, snapshot) {
+//             final provider = Provider.of<GoogleSignInProvider>(context);
+//
+//             if (provider.isSigningIn!) {
+//               return buildLoading();
+//             } else if (snapshot.hasData) {
+//               return HomePage();
+//             } else {
+//               return LoginPage();
+//             }
+//           },
+//         ),
+//       );
+//
+//   Widget buildLoading() => Stack(
+//         fit: StackFit.expand,
+//         children: [
+//           CustomPaint(painter: BackgroundPainter()),
+//           Center(child: CircularProgressIndicator()),
+//         ],
+//       );
+// }
