43ed2284dc7c6831c93365a568a4eb15502a420c
lakshanmaheshka <maheshkalakshan@gmail.com>
2021-05-22 18:58:13 +0530

    added sample homescreen if app

--- lib/screens/nav_screen.dart
+++ lib/screens/nav_screen.dart
@@ -1,6 +1,12 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:rt_gem/widgets/isApp/bottom_navigation_view/bottom_bar_view.dart';
+import 'package:rt_gem/widgets/isApp/models/tabIcon_data.dart';
+import 'package:rt_gem/widgets/isApp/my_diary/my_diary_screen.dart';
+import 'package:rt_gem/widgets/isApp/traning/training_screen.dart';
 import 'package:rt_gem/widgets/widgets.dart';
 
+import '../theme.dart';
 import 'home_page.dart';
 
 class NavScreen extends StatefulWidget {
@@ -8,31 +14,52 @@ class NavScreen extends StatefulWidget {
   _NavScreenState createState() => _NavScreenState();
 }
 
-class _NavScreenState extends State<NavScreen> {
+
+
+class _NavScreenState extends State<NavScreen> with TickerProviderStateMixin  {
+  AnimationController? animationController;
+
+  List<TabIconData> tabIconsList = TabIconData.tabIconsList;
+
   final List<Widget> _screens = [
     HomePage(),
     Scaffold(),
-    Scaffold(),
-    Scaffold(),
-    Scaffold(),
-    Scaffold(),
+    MyDiaryScreen(),
+    TrainingScreen(),
   ];
   final List<IconData> _icons = const [
     Icons.home,
     Icons.ondemand_video,
     Icons.home,
     Icons.ondemand_video,
-    Icons.home,
-    Icons.menu,
   ];
   int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    tabIconsList.forEach((TabIconData tab) {
+      tab.isSelected = false;
+    });
+    tabIconsList[0].isSelected = true;
+
+    animationController = AnimationController(
+        duration: const Duration(milliseconds: 600), vsync: this);
+    tabBody = MyDiaryScreen(animationController: animationController);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController!.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
     return DefaultTabController(
       length: _icons.length,
-      child: Scaffold(
+      child: kIsWeb ? Scaffold(
         appBar: Responsive.isDesktop(context)
             ? PreferredSize(
                 preferredSize: Size(screenSize.width, 100.0),
@@ -46,7 +73,12 @@ class _NavScreenState extends State<NavScreen> {
             : null,
         body: IndexedStack(
           index: _selectedIndex,
-          children: _screens,
+          children: <Widget>[
+            HomePage(),
+            Scaffold(),
+            MyDiaryScreen(animationController: animationController),
+            TrainingScreen(animationController: animationController)
+          ],
         ),
         bottomNavigationBar: !Responsive.isDesktop(context)
             ? Container(
@@ -59,7 +91,109 @@ class _NavScreenState extends State<NavScreen> {
                 ),
               )
             : const SizedBox.shrink(),
+      ) : Scaffold(
+        backgroundColor: Colors.transparent,
+        body: FutureBuilder<bool>(
+          future: getData(),
+          builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+            if (!snapshot.hasData) {
+              return const SizedBox();
+            } else {
+              return Stack(
+                children: <Widget>[
+                  tabBody,
+                  bottomBar(),
+                ],
+              );
+            }
+          },
+        ),
       ),
     );
   }
+
+  Future<bool> getData() async {
+    await Future<dynamic>.delayed(const Duration(milliseconds: 200));
+    return true;
+  }
+
+  Widget tabBody = Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            colors: <Color>[
+              CustomTheme.loginGradientStart,
+              CustomTheme.loginGradientEnd
+            ],
+            begin: FractionalOffset(0.0, 0.0),
+            end: FractionalOffset(1.0, 1.0),
+            stops: <double>[0.0, 1.0],
+            tileMode: TileMode.clamp),
+      ),
+    );
+
+  Widget bottomBar() {
+    return Column(
+      children: <Widget>[
+        const Expanded(
+          child: SizedBox(),
+        ),
+        BottomBarView(
+          tabIconsList: tabIconsList,
+          addClick: () {},
+          changeIndex: (int index) {
+
+            switch (index) {
+              case 0:
+                animationController!.reverse().then<dynamic>((data) {
+                  if (!mounted) {
+                    return;
+                  }
+                  setState(() {
+                    tabBody =
+                        MyDiaryScreen(animationController: animationController);
+                  });
+                });
+                break;
+              case 1:
+                animationController!.reverse().then<dynamic>((data) {
+                  if (!mounted) {
+                    return;
+                  }
+                  setState(() {
+                    tabBody =
+                        TrainingScreen(animationController: animationController);
+                  });
+                });
+                break;
+              case 2:
+                animationController!.reverse().then<dynamic>((data) {
+                  if (!mounted) {
+                    return;
+                  }
+                  setState(() {
+                    tabBody =
+                        HomePage();
+                  });
+                });
+                break;
+              case 3:
+                animationController!.reverse().then<dynamic>((data) {
+                  if (!mounted) {
+                    return;
+                  }
+                  setState(() {
+                    tabBody =
+                        TrainingScreen(animationController: animationController);
+                  });
+                });
+                break;
+            }
+          },
+        ),
+      ],
+    );
+  }
 }
+
+
+
